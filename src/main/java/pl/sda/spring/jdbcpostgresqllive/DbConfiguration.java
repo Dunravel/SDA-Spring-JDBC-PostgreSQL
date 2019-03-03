69e87525591a147271646ba47cb13ebe95ab2622
Marcin Iskra <dunravel@gmail.com>
2019-03-03 11:54:14 +0100

    Adding DB configuration

---
+++ src/main/java/pl/sda/spring/jdbcpostgresqllive/DbConfiguration.java
@@ -0,0 +1,22 @@
+package pl.sda.spring.jdbcpostgresqllive;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DbConfiguration {
+
+    @Bean
+    public DataSource dataSource(){
+        return null;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource){
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        jdbcTemplate.setDataSource(dataSource);
+    }
+}
