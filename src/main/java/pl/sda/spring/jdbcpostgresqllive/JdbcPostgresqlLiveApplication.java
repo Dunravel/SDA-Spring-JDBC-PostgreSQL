a22ee70c719a914b55462aa277302b1490a8f3b1
Marcin Iskra <dunravel@gmail.com>
2019-03-03 11:53:28 +0100

    Initial commit

---
+++ src/main/java/pl/sda/spring/jdbcpostgresqllive/JdbcPostgresqlLiveApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.spring.jdbcpostgresqllive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcPostgresqlLiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdbcPostgresqlLiveApplication.class, args);
+    }
+
+}
