1468ea288cee25735c5b036458a5eb56cd4baedf
Marcin Iskra <dunravel@gmail.com>
2019-03-03 12:06:30 +0100

    Added configuration for DB connection

--- src/main/java/pl/sda/spring/jdbcpostgresqllive/DbConfiguration.java
+++ src/main/java/pl/sda/spring/jdbcpostgresqllive/DbConfiguration.java
@@ -1,22 +1,32 @@
 package pl.sda.spring.jdbcpostgresqllive;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class DbConfiguration {
+    @Value("${driver.name}")
+    private String driverName;
 
     @Bean
     public DataSource dataSource(){
-        return null;
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(driverName);
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/cats");
+        dataSource.setUsername("psql_admin");
+        dataSource.setPassword("Dun123");
+        return dataSource;
     }
 
     @Bean
     public JdbcTemplate jdbcTemplate(DataSource dataSource){
         JdbcTemplate jdbcTemplate = new JdbcTemplate();
         jdbcTemplate.setDataSource(dataSource);
+        return jdbcTemplate;
     }
 }
