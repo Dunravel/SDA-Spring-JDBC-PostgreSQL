b83125c4b855acbd7d57f6d09811ef8bd90eac89
Marcin Iskra <dunravel@gmail.com>
2019-03-03 15:14:15 +0100

    Added function to get users containg text in first name

--- src/main/java/pl/sda/spring/jdbcpostgresqllive/UserRepository.java
+++ src/main/java/pl/sda/spring/jdbcpostgresqllive/UserRepository.java
@@ -2,7 +2,10 @@ package pl.sda.spring.jdbcpostgresqllive;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface UserRepository extends CrudRepository<User, Long> {
 
     User getUserByFirstName(String name);
+    List<User> getUsersByFirstNameContains(String text);
 }
